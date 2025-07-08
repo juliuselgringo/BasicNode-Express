6cdd4324d1eb2093121077186f59e1b6d6ec7abb
julius_el_gringo <julien.rousselot@hotmail.fr>
2025-07-08 12:26:50 +0200

    variables in .env

--- myApp.js
+++ myApp.js
@@ -1,12 +1,26 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
 
+app.use('/public', express.static(__dirname + '/public'))
 
+app.get("/json", (req,res) => {
+    response = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        response = response.toUpperCase();
+    }
+    else{
+        response = "hello json";
+    }
+    res.json({"message": response});
+})
 
-
-
-
+//lancer le serveur : node --watch server.js
 
 
 
