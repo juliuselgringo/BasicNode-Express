3ee5d523ac4dcb1e9334365c438eddd0d4bdf802
julius_el_gringo <julien.rousselot@hotmail.fr>
2025-07-08 16:51:23 +0200

    feat: user data by get & doc the route

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ require('dotenv').config()
 
 let express = require('express');
 let app = express();
+// app.get pour GET //app.post pour POST
 
+/**
+ * on affiche des infos dans la console du serveur
+ */
 app.use(function middleware(req, res, next){
     const response = req.method + " " + req.path + " - " + req.ip;
     console.log(response);
@@ -20,11 +24,18 @@ app.get(
     }
 )
 
+/**
+ * A la racine on affiche index.html
+ */
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
+/**
+ * ici permet d'appliquer le style.css contenu dans /public
+ */
 app.use('/public', express.static(__dirname + '/public'))
+
 
 app.get("/json", (req,res) => {
     response = "Hello json";
@@ -35,6 +46,16 @@ app.get("/json", (req,res) => {
         response = "hello json";
     }
     res.json({"message": response});
+})
+
+/**
+* récupérer des données de l'utilisateur
+* pour tester: utiliser localhost:3000/cplusclair/echo
+* la page affichera le json: {"echo": "cplusclair"}
+*/
+app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({echo : word});
 })
 
 
